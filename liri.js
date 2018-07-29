ff7a58b1f6bd90d5534ac64c397faea4742c76ab
Tyson <tys_p@yahoo.ca>
2018-07-29 13:07:17 -0400

    Completed OMBD and Spotify requests, need to finish Twitter call

--- liri.js
+++ liri.js
@@ -1,10 +1,11 @@
 //initialize required modules
+require("dotenv").config();
+var keys = require("./keys");
 var request = require("request");
 var Spotify = require('node-spotify-api');
 var Twitter = require('twitter');
-var fs = require("fs");
-var keys = require("./keys");
-require("dotenv").config();
+//var fs = require("fs");
+//var inquirer = require("inquirer");
 var spotify = new Spotify(keys.spotify);
 //var client = new Twitter(keys.twitter);
 
@@ -15,46 +16,76 @@ var nodeArr = [];
 for (var i = 3; i < nodeArg.length; i++) {
     nodeArr.push(nodeArg[i]);
 };
-var nodeValue = nodeArr.join(" ");
-
-function tweets() {
-    //content
+var nodeValue = nodeArr.join("+");
+var queryUrl = "http://www.omdbapi.com/?t=" + nodeValue + "&y=&plot=short&apikey=trilogy";
+var options = {  
+    url: queryUrl,
+    method: "GET",
 };
 
-function spotify() {
+function tweets() {
+    console.log("tweetsDone");
+};
+
+function spotifySong() {
+    //console.log("spotifyDone");
     spotify.search({ 
-        type: 'artist OR album OR track',
+        type: "track",
         query: nodeValue
     }, function(err, data) {
         if (err) {
-          console.log('Error occurred: ' + err);
-        }
-        console.log(data); 
+          console.log("Error occurred: " + err);
+        } else {
+            //console.log("*********************************")
+            //console.log(JSON.stringify(data, null, 2));
+            //console.log("*********************************")
+            for (i = 0; i < data.tracks.items.length; i++) {
+                console.log("Result # " + (i + 1));
+                console.log("Artist: " + data.tracks.items[i].artists[0].name);
+                console.log("Track: " + data.tracks.items[i].name);
+                console.log("Album: " + data.tracks.items[i].album.name);
+                console.log("Explicit content: " + data.tracks.items[i].explicit);
+                console.log("Link to song: " + data.tracks.items[i].external_urls.spotify);
+                console.log("*********************************");
+            };
+        }; 
     });
 };
 
 function movie() {
-    //content
+    request(options, function(err, res, body) {
+        if (err){
+            console.log("Error occurred: " + err);
+        } else {
+            var json = JSON.parse(body);
+            console.log("Title: " + json.Title);
+            console.log("Director: " + json.Director);
+            console.log("Release date: " + json.Released);
+            console.log("Rating: " + json.Rated);
+            console.log("Language: " + json.Language);
+            console.log("Plot: " + json.Plot);
+            console.log("Cast: " + json.Actors);
+            console.log("Ratings: \n  IMDB: " + json.Ratings[0].Value + "\n  Rotten Tomatoes: " + json.Ratings[1].Value + "\n  Metacritic: " + json.Ratings[2].Value);
+        };
+    });
 };
 
 function command() {
-    //content
+    console.log("commandDone");
 };
 
-switch (action) {
-    case "my-tweets":
+if (action == "my-tweets") {
+    console.log("tweets");
     tweets();
-    break;
-
-    case "spotify-this-song":
-    spotify();
-    break;
-
-    case "movie-this":
+} else if (action == "spotify-this-song") {
+    //console.log("spotify");
+    spotifySong();
+} else if (action == "movie-this") {
+    console.log("movie");
     movie();
-    break;
-
-    case "do-what-it-says":
+} else if (action == "do-what-it-says") {
+    console.log("command");
     command();
-    break;
+} else {
+    console.log("Invalid entry");
 };
